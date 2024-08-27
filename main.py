f4138da70836194a13098199b6aa8eaf62fc6806
Camilla <p72649cm@postrad,manchester.ac.uk>
2024-08-27 16:07:31 +0100

    main file added

---
+++ main.py
@@ -0,0 +1,34 @@
+from nbformat import read
+import yaml
+
+from ExtractFrames import extract_frames_main
+from GettingPredictions import PrepareDataForModel
+
+
+def read_config():
+
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    return config
+
+def run_main(extract_frames, get_predictions):
+
+    extract_frames = '2'
+
+    if extract_frames:
+        extract_frames_main()
+    
+    if get_predictions:
+        PrepareDataForModel()
+
+
+
+if __name__=='__main__':
+
+    config = read_config()
+
+    extract_frames = True
+    get_predictions= False
+    run_main(extract_frames, get_predictions)
+
